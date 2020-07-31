7a89dd50d028bc0c64c9aed2ef15e39f786a4c8e
yegorfsl <yegorfarenbrukhsl@gmail.com>
2020-07-31 22:35:35 +0300

    Initial Commit

---
+++ BehancePP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BehancePP
+//
+//  Created by apple on 7/31/20.
+//  Copyright © 2020 GQt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
